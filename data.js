cf71251111b8ac5ccd0ec9e9af1b56270277890e
Romain So <so.romain77@gmail.com>
2026-04-04 16:08:46 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,321 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-vue-globale",
+      "name": "Vue Globale",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.105486013214314,
+          "pitch": 0.15354453458184736,
+          "rotation": 3.141592653589793,
+          "target": "2-port-de-la-darse",
+          "type": "zoom_in"
+        },
+        {
+          "yaw": -0.20103292818974694,
+          "pitch": -0.05622296363617174,
+          "rotation": 3.141592653589793,
+          "target": "3-citadelle",
+          "type": "zoom_in"
+        },
+        {
+          "yaw": 0.20670233187704312,
+          "pitch": -0.10907161229858175,
+          "rotation": 3.141592653589793,
+          "target": "1-ville",
+          "type": "zoom_in"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-ville",
+      "name": "Ville",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2600338446068076,
+          "pitch": 0.006528602863001609,
+          "rotation": 3.141592653589793,
+          "target": "3-citadelle",
+          "type": "zoom_in"
+        },
+        {
+          "yaw": -1.4777100982297533,
+          "pitch": 0.006643605157915289,
+          "rotation": 3.141592653589793,
+          "target": "2-port-de-la-darse",
+          "type": "zoom_in"
+        },
+        {
+          "yaw": -2.788800522595036,
+          "pitch": -0.04735408440035194,
+          "rotation": 0,
+          "target": "0-vue-globale"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.077278248580118,
+          "pitch": -0.030581346021556044,
+          "title": "Église",
+          "text": "<a onclick=\"openVideo('https://www.youtube.com/watch?v=N0xNetKX1g0')\">Voir la vidéo</a>"
+        },
+        {
+          "yaw": -0.06005469199459057,
+          "pitch": 0.08607060610490791,
+          "title": "Cour",
+          "text": "<a onclick=\"openVideo('https://www.youtube.com/watch?v=CUQSmRZ_3QI')\">Voir la vidéo</a>"
+        },
+        {
+          "yaw": -1.4642425194515596,
+          "pitch": 0.2454922002605322,
+          "title": "Point de vue",
+          "text": "<a onclick=\"openVideo('https://www.youtube.com/watch?v=tr-MoQB0LWc')\">Voir la vidéo</a>"
+        },
+        {
+          "yaw": 1.0527244940195715,
+          "pitch": 0.21312191010081705,
+          "title": "Rue",
+          "text": "<a onclick=\"openVideo('https://www.youtube.com/watch?v=zzzdsURD-iU')\">Voir la vidéo</a>"
+        },
+        {
+          "yaw": -0.7805090216488626,
+          "pitch": -0.06983251984459571,
+          "title": "Office de tourisme",
+          "text": "<a onclick=\"openVideo('https://www.youtube.com/watch?v=MbX-M7AXzp8')\">Voir la vidéo</a>"
+        },
+        {
+          "yaw": -0.4669369141039468,
+          "pitch": 0.7575816512001339,
+          "title": "Port",
+          "text": "<a onclick=\"openVideo('https://www.youtube.com/watch?v=A6Xj7GYFirI')\">Voir la vidéo</a>"
+        },
+        {
+          "yaw": 1.61801781149257,
+          "pitch": 0.15923536217215428,
+          "title": "Plage",
+          "text": "<a onclick=\"openVideo('https://www.youtube.com/watch?v=fsxJJpX0AT4')\">Voir la vidéo</a>"
+        },
+        {
+          "yaw": 0.5327244940195715,
+          "pitch": 0.02312191010081705,
+          "title": "Centre ville",
+          "text": "<a onclick=\"openVideo('https://www.youtube.com/watch?v=kCDEPQYYXQ4')\">Voir la vidéo</a>"
+        },
+        {
+          "yaw": 0.3327244940195715,
+          "pitch": -0.10000001010081705,
+          "title": "Escaliers",
+          "text": "<a onclick=\"openVideo('https://www.youtube.com/watch?v=4oml9JGv-u0')\">Voir la vidéo</a>"
+        }
+      ]
+    },
+    {
+      "id": "2-port-de-la-darse",
+      "name": "Port de la Darse",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8738056040282354,
+          "pitch": 0.10574068852405816,
+          "rotation": 9.42477796076938,
+          "target": "3-citadelle",
+          "type": "zoom_in"
+        },
+        {
+          "yaw": 1.7174560558499792,
+          "pitch": -0.05440401782667159,
+          "rotation": 3.141592653589793,
+          "target": "1-ville",
+          "type": "zoom_in"
+        },
+        {
+          "yaw": 2.9121374290828026,
+          "pitch": 0.01859419919341576,
+          "rotation": 0,
+          "target": "0-vue-globale"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.0559912179259303,
+          "pitch": 0.7888027664926476,
+          "title": "Bateau",
+          "text": "<a onclick=\"openVideo('https://www.youtube.com/watch?v=n81_j6HHkQQ')\">Voir la vidéo</a>"
+        },
+        {
+          "yaw": -0.3461749650386672,
+          "pitch": -0.09262791820646754,
+          "title": "Jardin Beaudoin",
+          "text": "<a onclick=\"openVideo('https://www.youtube.com/watch?v=qXrRrmwpRZ0')\">Voir la vidéo</a>"
+        },
+        {
+          "yaw": -0.7259182078174415,
+          "pitch": -0.043709286882080534,
+          "title": "Calle sèche",
+          "text": "<a onclick=\"openVideo('https://www.youtube.com/watch?v=LJaiEKYRVEE')\">Voir la vidéo</a>"
+        },
+        {
+          "yaw": -0.17165630723275704,
+          "pitch": -0.036942430156337025,
+          "title": "Chantier naval",
+          "text": "<a onclick=\"openVideo('https://www.youtube.com/watch?v=9xRNuSoRssI')\">Voir la vidéo</a>"
+        },
+        {
+          "yaw": 0.32546557642335827,
+          "pitch": 0.08966503775692836,
+          "title": "Caserne",
+          "text": "<a onclick=\"openVideo('https://www.youtube.com/watch?v=ejb87lm89Z8')\">Voir la vidéo</a>"
+        }
+      ]
+    },
+    {
+      "id": "3-citadelle",
+      "name": "Citadelle",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2011614802113257,
+          "pitch": 0.030782943743282942,
+          "rotation": 3.141592653589793,
+          "target": "2-port-de-la-darse",
+          "type": "zoom_in"
+        },
+        {
+          "yaw": 0.8310790915952246,
+          "pitch": -0.1414698546258002,
+          "rotation": 3.141592653589793,
+          "target": "1-ville",
+          "type": "zoom_in"
+        },
+        {
+          "yaw": 2.5592233867526346,
+          "pitch": 0.10007883633807069,
+          "rotation": 0,
+          "target": "0-vue-globale"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4160568588386617,
+          "pitch": 0.18657967315455792,
+          "title": "Jardin",
+          "text": "<a onclick=\"openVideo('https://www.youtube.com/watch?v=zDfg84E80tw')\">Voir la vidéo</a>"
+        },
+        {
+          "yaw": 0.006001397787585461,
+          "pitch": 0.29373932904853817,
+          "title": "Échauguettes",
+          "text": "<a onclick=\"openVideo('https://www.youtube.com/watch?v=mcuOFxYrFzg')\">Voir la vidéo</a>"
+        },
+        {
+          "yaw": 0.4569268722741402,
+          "pitch": 0.01010561362173057,
+          "title": "Pont",
+          "text": "<a onclick=\"openVideo('https://www.youtube.com/watch?v=USUoiBc17QQ')\">Voir la vidéo</a>"
+        },
+        {
+          "yaw": 0.06615309808378633,
+          "pitch": 0.06883743781764196,
+          "title": "Hotel de ville",
+          "text": "<a onclick=\"openVideo('https://www.youtube.com/watch?v=TBwGXfEVvEY')\">Voir la vidéo</a>"
+        },
+        {
+          "yaw": -0.017484049137150848,
+          "pitch": -0.07299574620958893,
+          "title": "Auditorium",
+          "text": "<a onclick=\"openVideo('lien')\">Voir la vidéo</a>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
